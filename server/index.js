027b4a0150e544b65f8b7b8d6fdf0325f64e1279
michaelscoggins <rhythmghosts@gmail.com>
2021-05-14 18:20:38 -0500

    changed db name

--- server/index.js
+++ server/index.js
@@ -4,8 +4,6 @@ const express = require("express");
 const cors = require("cors");
 const session = require("express-session");
 const path = require("path");
-// const bodyParser = require("body-parser");
-// const cookieParser = require("cookie-parser");
 
 //routers
 const routers = require("./routers/routers");
@@ -14,12 +12,7 @@ const routers = require("./routers/routers");
 //more initializing
 const app = express();
 const port = process.env.PORT || 4001;
-// app.use(express.static("../build"));
 app.use(express.static(path.join(__dirname, "../build")));
-// const corsOptions = {
-//   origin: "http://localhost:3000",
-//   optionsSuccessStatus: 200,
-// };
 
 app.use(
   session({
@@ -28,13 +21,9 @@ app.use(
     saveUninitialized: true,
   })
 );
-// app.use(cookieParser());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use("/", express.static("../build"), cors(), routers);
 app.use(cors(), routers);
-// app.use('/auth', cors(corsOptions), authRouter)
+
 app.get("/", (req, res) => {
   res.send("theMVPshop");
 });
--- server/sql/connection.js
+++ server/sql/connection.js
@@ -1,28 +1,28 @@
-const mysql = require('mysql');
-require ('dotenv').config();
+const mysql = require("mysql");
+require("dotenv").config();
 
 class Connection {
-    constructor() {
-        if (!this.pool) {
-            console.log('creating mysql connection...');
+  constructor() {
+    if (!this.pool) {
+      console.log("creating mysql connection...");
 
-            const config = {
-                connectionLimit: 100,
-                host: process.env.DB_HOST,
-                port: 3306,
-                user: process.env.DB_USER,
-                password: process.env.DB_PASS,
-                database: 'db9201_MVP'
-            }
+      const config = {
+        connectionLimit: 100,
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "a3zl5bffh54p5tff",
+      };
 
-            this.pool = mysql.createPool(config);
+      this.pool = mysql.createPool(config);
 
-            return this.pool;
-        }
-
-        return this.pool
+      return this.pool;
     }
+
+    return this.pool;
+  }
 }
 
-const instance = new Connection()
+const instance = new Connection();
 module.exports = instance;
