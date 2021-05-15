70c72cc31d9b4a31a8c8543d7ce258dbc14942c7
michaelscoggins <rhythmghosts@gmail.com>
2021-05-14 20:48:50 -0500

    un-nested server.js

--- src/server.js
+++ src/server.js
@@ -6,13 +6,13 @@ const session = require("express-session");
 const path = require("path");
 
 //routers
-const routers = require("./routers/routers");
+const routers = require("../server/routers/routers");
 // const authRouter = require('./routers/auth');
 
 //more initializing
 const app = express();
 const port = process.env.PORT || 4001;
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "build")));
 // app.get("*", function (req, res) {
 //   res.sendFile(path.join(__dirname, "../build", "index.html"));
 // });
