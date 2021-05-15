3228f846af92e2843880aa5bae541224ea076a63
Keith Kritselis <kkritselis@gmail.com>
2021-05-14 20:07:58 -0500

    fixTest

--- server/index.js
+++ server/index.js
@@ -27,13 +27,6 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(), routers);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-app.use(routes);
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.get("/", (req, res) => {
   res.send("theMVPshop");
