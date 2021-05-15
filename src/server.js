00aa82ce3fa264e9206b49df85f94298aa7d1baa
michaelscoggins <rhythmghosts@gmail.com>
2021-05-14 21:45:34 -0500

    forgot to uncomment out require(path)

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const cors = require("cors");
 // const session = require("express-session");
-// const path = require("path");
+const path = require("path");
 
 //routers
 const routers = require("./server/routers/routers");
