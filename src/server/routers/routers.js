3d01676a9b3615e2fd4e8c28bd116dbdea674e7c
Keith Kritselis <kkritselis@gmail.com>
2021-05-18 17:07:13 -0500

    login

--- src/server/routers/routers.js
+++ src/server/routers/routers.js
@@ -37,4 +37,12 @@ router.post("/devlog", devlogControllers.createDevlog);
 router.delete("/devlog/:id", devlogControllers.deleteDevlogById);
 router.put("/devlog", devlogControllers.updateDevlogById);
 
+// login
+router.post('/login',function(req,res){
+  var user_name = req.body.user;
+  var password = req.body.password;
+  console.log("User name = "+user_name+", password is "+password);
+  res.end("yes");
+});
+
 module.exports = router;
