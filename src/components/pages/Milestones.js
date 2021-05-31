32f54f72b825a0cdef9193589835a879ca336873
Keith Kritselis <kkritselis@gmail.com>
2021-05-31 16:25:15 -0500

    style fun

--- src/components/pages/Milestones.js
+++ src/components/pages/Milestones.js
@@ -146,8 +146,8 @@ function Milestones() {
 
   return (
     <>
-      <div  className="pb-3 mb-2"style={{ backgroundColor: "rgba(0,0,0,.25", margin: "auto", border: "solid 3px var(--indigo)", width:"80%", borderRadius: "30px 30px 0 0" }}>
-        <div className="mileContainer pt-2 pb-2 mb-3" style={{ backgroundColor: "var(--indigo)", borderRadius: "25px 25px 0 0", filter: "drop-shadow(0 10px 0.05rem rgba(0,0,0,.55)" }}>
+      <div  className="pb-3 mb-2"style={{ backgroundColor: "rgba(0,0,0,.25", margin: "auto", border: "solid 3px var(--indigo)", width:"100%", borderRadius: "30px 30px 0 0" }}>
+        <div className="mileContainer pt-2 pb-2 mb-3" style={{ backgroundColor: "var(--indigo)", color: "var(--light)", borderRadius: "25px 25px 0 0", filter: "drop-shadow(0 10px 0.05rem rgba(0,0,0,.55)" }}>
           <Container className="d-flex p-6 justify-content-evenly mt-2">
             <MilestonesProjectSelectModal
               fromMilestones={true}
